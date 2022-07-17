583beb80f1700b6eeb7721e1e2bd3b2b2f2e07be
hideki-abe <hideki-abe@hotmail.com>
2022-07-17 20:13:34 -0300

    configuração de microserviço

---
+++ notificaca-vaga/src/test/java/com/notificacaovaga/notificacavaga/NotificacaVagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notificacaovaga.notificacavaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaVagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
